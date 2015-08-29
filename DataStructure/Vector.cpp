71f8c6a2a36600edc74bbec47ec812a0cc732e46
Yang.L <LovingYoung0710@gmail.com>
2015-08-29 12:58:20 +0800

    init

---
+++ DataStructure/Vector.cpp
@@ -0,0 +1,5 @@
+//
+// Created by liuyang on 15-8-29.
+//
+
+#include "Vector.h"
---
+++ DataStructure/Vector.h
@@ -0,0 +1,15 @@
+//
+// Created by liuyang on 15-8-29.
+//
+
+#ifndef SHANGHAI_SUBWAY_VECTOR_H
+#define SHANGHAI_SUBWAY_VECTOR_H
+
+
+template <class T>;
+class Vector<class T> {
+
+};
+
+
+#endif //SHANGHAI_SUBWAY_VECTOR_H
